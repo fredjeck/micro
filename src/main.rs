294b1c30f7d1d2c59c6ad66c7db53e8a3e549767
FredJeck <frederic.jecker@gmail.com>
2021-01-24 21:24:39 +0100

    Added warp server

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,52 @@ mod filesystem;
 use convert::publish;
 use filesystem::{make_watcher, walk_dir};
 use log::info;
-use simple_logger::SimpleLogger;
+use pretty_env_logger;
 use std::path::Path;
 use std::{env, error::Error, process::Command};
+use warp::Filter;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    SimpleLogger::new().init()?;
-
-    make_watcher(env::current_dir()?, handle_path_change, true, 1000);
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
 
     make_watcher(
-        env::current_dir()?.join("templates"),
+        env::current_dir().unwrap().join("blog"),
+        handle_path_change,
+        true,
+        1000,
+    );
+
+    make_watcher(
+        env::current_dir().unwrap().join("templates"),
         handle_template_change,
         true,
         1000,
-    )
-    .join()
-    .unwrap();
+    );
 
-    Ok(())
+    // dir already requires GET...
+    let blog = warp::get()
+        .and(warp::fs::dir("./blog/"));
+
+    warp::serve(blog).run(([127, 0, 0, 1], 3030)).await;
 }
 
 fn handle_template_change(p: &Path) {
     || -> Result<(), Box<dyn Error>> {
-        info!("The template {} changed, re-publishing pages", p.to_str().unwrap());
+        info!(
+            "The template {} changed, re-publishing pages",
+            p.to_str().unwrap()
+        );
         if p.extension().unwrap().to_str().unwrap() != "html" {
             return Ok(());
         };
         // TODO make this a little bit cleaner and only republish the ones using the template which changed
-        walk_dir(env::current_dir()?.as_path(), "md", handle_path_change, true)?;
+        walk_dir(
+            env::current_dir()?.as_path(),
+            "md",
+            handle_path_change,
+            true,
+        )?;
 
         Ok(())
     }()
