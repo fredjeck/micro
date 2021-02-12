c3fb328a5beef85c8bc453ecfd40771f21f1c1f5
FredJeck <frederic.jecker@gmail.com>
2021-02-12 20:01:53 +0100

    More fun

--- src/devserver.rs
+++ src/devserver.rs
@@ -1,11 +1,13 @@
-use std::{collections::HashMap, path::PathBuf, sync::Arc, time::Duration};
-
-use futures::{Future, SinkExt, StreamExt};
+use futures::{SinkExt, StreamExt};
 use log::{debug, error, info};
-use tokio::{sync::{
-    mpsc::{self, UnboundedReceiver, UnboundedSender},
-    RwLock,
-}, time::sleep};
+use std::{collections::HashMap, path::PathBuf, sync::Arc, time::Duration};
+use tokio::{
+    sync::{
+        mpsc::{self, UnboundedReceiver, UnboundedSender},
+        RwLock,
+    },
+    time::sleep,
+};
 use uuid::Uuid;
 use warp::{
     ws::{Message, WebSocket, Ws},
@@ -38,7 +40,13 @@ impl DevServer {
 
     /// Starts the local development web server, serving the content of __www_root__.
     /// If __open_in_browser__ is set to true, the system's default browser will be openened at the specified __root_url__
-    pub async fn serve(&self, www_root: PathBuf, port: u16, open_in_browser: bool) {
+    pub async fn serve(
+        &self,
+        www_root: PathBuf,
+        port: u16,
+        open_in_browser: bool,
+        root_url: Option<String>,
+    ) {
         if !www_root.exists() {
             panic!(format!(
                 "Cannot serve content from '{:#?}': the path does not exist",
@@ -52,16 +60,22 @@ impl DevServer {
         let uplink = warp::path("uplink")
             .and(warp::ws())
             .and(warp::any().map(move || connexions.clone()))
-            .and_then(handle_ws);
+            .and_then(register_ws_handler);
 
         let root = warp::get().and(warp::fs::dir(www_root));
 
         let server = warp::serve(root.or(uplink));
 
+        let path = match root_url {
+            Some(u) => u,
+            None => "/".to_string(),
+        };
+
         if open_in_browser {
             tokio::task::spawn(async move {
+                // Lets delay the browser's opening
                 sleep(Duration::from_millis(5000)).await;
-                let url = format!("http://localhost:{}", &port);
+                let url = format!("http://localhost:{}{}", &port, path);
                 webbrowser::open(&url).unwrap();
             });
         }
@@ -69,23 +83,27 @@ impl DevServer {
         server.run(([127, 0, 0, 1], port)).await
     }
 
+    /// Returns the list clients connected via WebSocket
     pub fn clients(&self) -> Clients {
         self.clients.clone()
     }
 }
 
-pub async fn handle_ws(ws: Ws, clients: Clients) -> Result<impl Reply> {
+/// Registers the WebSocket connection handler
+async fn register_ws_handler(ws: Ws, clients: Clients) -> Result<impl Reply> {
     Ok(ws.on_upgrade(move |socket| con_client_connected(socket, clients)))
 }
 
-pub async fn con_client_connected(ws: WebSocket, clients: Clients) {
-    info!("Live preview instance connected");
+/// Called whenever a new client connects via websockets
+async fn con_client_connected(ws: WebSocket, clients: Clients) {
+    debug!("New WS client connection request received");
 
     let (mut client_ws_sender, mut client_ws_rcv) = ws.split();
     let (client_sender, mut client_rcv): (UnboundedSender<Message>, UnboundedReceiver<Message>) =
         tokio::sync::mpsc::unbounded_channel();
     let id = Uuid::new_v4().to_string();
 
+    // This should be coverd by a forward method, wich existed at some point - need to investigate
     tokio::task::spawn(async move {
         while let Some(result) = client_rcv.recv().await {
             let _ = client_ws_sender.send(result).await;
@@ -99,13 +117,13 @@ pub async fn con_client_connected(ws: WebSocket, clients: Clients) {
 
     clients.write().await.insert(client.id.clone(), client);
 
-    info!("Live preview instance '{}' is now registered", &id);
+    info!("Live preview instance '{}' now registered", &id);
 
     while let Some(result) = client_ws_rcv.next().await {
         let msg = match result {
             Ok(msg) => msg,
             Err(e) => {
-                error!("Error receiving ws message for id: {}): {}", &id, e);
+                error!("Error receiving message from client '{}': {}", &id, e);
                 break;
             }
         };
@@ -116,21 +134,24 @@ pub async fn con_client_connected(ws: WebSocket, clients: Clients) {
     info!("Live preview instance '{}' is now disconnected", &id);
 }
 
+/// Triggered whenever a message is received via WebSocket
 async fn client_msg(id: &str, msg: Message, clients: &Clients) {
     let mut locked = clients.write().await;
     if let Some(v) = locked.get_mut(id) {
+        debug!("Message received from '{}': {:#?}", v.id, msg);
         if let Some(sender) = &v.sender {
-            let _ = sender.send(Message::text("Micro live preview"));
+            let _ = sender.send(Message::text("Pong !"));
         }
     }
 }
 
-pub async fn notify_clients(clients: &Clients, path: String) {
+/// Sends a message to the provided list of WebSocket connect clients
+pub async fn send_message(clients: &Clients, message: String) {
     clients.read().await.iter().for_each(|(_, client)| {
-        debug!("Notifiying client '{}' for '{}'", client.id, path);
+        debug!("Notifiying client '{}' for '{}'", client.id, message);
         if let Some(sender) = &client.sender {
             //let _ = sender.send(Message::text(path.clone()));
-            let _ = sender.send(Message::text(path.clone()));
+            let _ = sender.send(Message::text(message.clone()));
         }
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-mod watcher;
 mod devserver;
+mod watcher;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
 
 use devserver::DevServer;
 use log::info;
-use tokio::{join, sync::mpsc::{Receiver, Sender}};
+use tokio::{
+    join,
+    sync::mpsc::{Receiver, Sender},
+};
 
 #[tokio::main]
 async fn main() {
@@ -18,19 +21,26 @@ async fn main() {
     let root_watcher = watcher::make_fs_watcher(root_path.clone(), sender.clone(), true, 1000);
 
     let templates_path = PathBuf::from("G:\\Workspaces\\Rust\\micro\\templates\\");
-    let templates_watcher = watcher::make_fs_watcher(templates_path, sender, true, 1000);
- 
+    let templates_watcher = watcher::make_fs_watcher(templates_path.clone(), sender, true, 1000);
 
     let server = DevServer::new();
-    let server_task = server.serve(root_path, 4200, true);
-    let clients = server.clients(); 
+    let server_task = server.serve(root_path.clone(), 4200, true, None);
+    let clients = server.clients();
 
     let consumer = tokio::task::spawn(async move {
         loop {
             let message = receiver.recv().await;
             if let Some(text) = message {
-                info!("File {} changed", text);
-                devserver::notify_clients(&clients, text).await;
+                info!("File {} changed", &text);
+                let file_path = Path::new(&text);
+                if file_path.starts_with(&templates_path) {
+                    continue;
+                }
+
+                if let Ok(p) = file_path.strip_prefix(&root_path){
+                    let str = p.to_str().unwrap();
+                    devserver::send_message(&clients, str.replace(MAIN_SEPARATOR, "/")).await;
+                }
             }
         }
     });
