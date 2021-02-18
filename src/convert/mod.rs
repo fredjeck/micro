1bf8385f0753b36a47648adf0044ef643a65c18f
FredJeck <frederic.jecker@gmail.com>
2021-02-18 11:58:52 +0100

    Refactoring still in progress,slowly getting there

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -15,14 +15,15 @@ use std::{
 };
 use tokio::sync::mpsc::Receiver;
 
-use template::{merge_template};
+use template::merge_template;
 
-pub async fn create_markdown_pipeline(
+// Unused for now, leave it here for documentation
+pub async fn _create_markdown_pipeline(
     mut receiver: Receiver<PathBuf>,
     templates_location: PathBuf,
 ) -> tokio::task::JoinHandle<()> {
     if !templates_location.exists() {
-        panic!(format!("Cannot initialize markdown pipeline : templates location {:#?} leads to a non existing path", templates_location).as_str());
+        panic!(format!("Cannot initialize markdown pipeline : templates location {:#?} leads to a non existing path", &templates_location).as_str());
     }
 
     tokio::task::spawn(async move {
@@ -32,137 +33,105 @@ pub async fn create_markdown_pipeline(
                 None => continue,
             };
 
-            debug!("Markdown pipeline : publishing {:#?}", &source);
-
-            if source.extension() != Some(OsStr::new("md")) {
-                debug!("{:#?} is not markdown...skipping", source);
-                continue;
+            match markdown_to_html(source,None, templates_location.clone()){
+                Ok(_) => {}
+                Err(e) => {
+                    error!("{}", e);
+                    continue;
+                }
             }
-
-            let mut markdown = Vec::new();
-            let mut source_file = match File::open(&source) {
-                Ok(handle) => handle,
-                Err(error) => {
-                    error!("The following error occurred while opening {:#?} : {:#?}, this file will be skipped", error, &source);
-                    continue;
-                }
-            };
-            match source_file.read_to_end(&mut markdown) {
-                Ok(_) => {}
-                Err(error) => {
-                    error!("The following error occurred while reading {:#?} : {:#?}, this file will be skipped", error, &source);
-                    continue;
-                }
-            };
-
-            let mut markdown_content = match String::from_utf8(markdown) {
-                Ok(m) => m,
-                Err(error) => {
-                    error!("UTF8 conversion error occured for {:#?} ...skipping : {:#?}, this file will be skipped", &source, error);
-                    continue;
-                }
-            };
-
-            let metadata = match MarkdownMetaData::extract(&mut markdown_content) {
-                Some(meta) => meta,
-                None => {
-                    error!("Unable to extract metadata from {:#?}, this file will be skipped", &source);
-                    continue;
-                }
-            };
-
-            // TODO : improve that, and keep a reference instead of re-instanciating it every time
-            let parser = Parser::new_ext( markdown_content.as_str(), Options::all());
-
-            let mut html = String::new();
-            html::push_html(&mut html, parser);
-
-            let mut template = Vec::new();
-            match template::load_template(&metadata.layout.to_string(), None,  &mut template){
-                Ok(_) => {}
-                Err(_) => {
-                    error!("Unable to load template {:#?} for {:#?}, this file will be skipped", metadata.layout, &source);
-                    continue;
-                }
-            };
-
-            let content = str::from_utf8(&template).unwrap_or("");
-
-            let document = merge_template(content, &metadata, &html);
-
-            let html_file = match File::create(source.with_extension("html")) {
-                Ok(handle) => handle,
-                Err(_) => continue
-            };
-
-            let mut writer = BufWriter::new(html_file);
-
-            match writer.write(document.as_bytes()){
-                Ok(_) => {}
-                Err(_) => {
-                    error!("Unable to write the rendered HTML file for {:#?}", &source);
-                    continue;
-                }
-            };
         }
     })
 }
 
-pub fn publish(source: PathBuf) -> Result<PathBuf, Box<dyn error::Error + Send + Sync>> {
+/// Converts the source markdown file (which needs to have a .md file extension) to HTML using a layout template specified via Metadata 
+/// If no destination is specified, the generated html file will be placed in the same path as the source file with the ".html" extension
+pub fn markdown_to_html(source: PathBuf, destination: Option<PathBuf>, templates_location: PathBuf) -> Result<PathBuf, Box<dyn error::Error + Send + Sync>> {
+    debug!("Publishing {:#?}", &source);
+
     if source.extension() != Some(OsStr::new("md")) {
-        bail!("For now only markdown files are supported");
+        bail!("{:#?} is not markdown...skipping", source);
     }
 
     let mut markdown = Vec::new();
-
     let mut source_file = match File::open(&source) {
         Ok(handle) => handle,
-        Err(error) => bail!(
-            "An error occured while accessing {:#?} for reading {}",
-            source,
-            error
-        ),
+        Err(error) => {
+            bail!("The following error occurred while opening {:#?} : {:#?}", error, &source);
+        }
     };
-
     match source_file.read_to_end(&mut markdown) {
         Ok(_) => {}
-        Err(error) => bail!("An error occured reading {:#?} content {}", source, error),
-    };
-
-    let mut markdown_content = str::from_utf8(&markdown)?.to_string();
-    let metadata = match MarkdownMetaData::extract(&mut markdown_content) {
-        Some(meta) => meta,
-        None => {
-            bail!("Missing or incomplete metadata for file  {:#?} ", source)
+        Err(error) => {
+            bail!("The following error occurred while reading {:#?} : {:#?}", error, &source);
         }
     };
 
-    println!("Metadata {}", metadata);
+    let mut markdown_content = match String::from_utf8(markdown) {
+        Ok(m) => m,
+        Err(error) => {
+            bail!("UTF8 conversion error occured for {:#?} ...skipping : {:#?}", &source, error);
+        }
+    };
 
+    let metadata = match MarkdownMetaData::extract(&mut markdown_content) {
+        Some(meta) => meta,
+        None => {
+            bail!(
+                "Unable to extract metadata from {:#?}",
+                &source
+            );
+        }
+    };
+
+    // TODO : improve that, and keep a reference instead of re-instanciating it every time
     let parser = Parser::new_ext(markdown_content.as_str(), Options::all());
 
     let mut html = String::new();
     html::push_html(&mut html, parser);
 
     let mut template = Vec::new();
-    template::load_template(&metadata.layout.to_string(), None,  &mut template)?;
+    match template::load_template(&metadata.layout.to_string(), Some(templates_location), &mut template) {
+        Ok(_) => {}
+        Err(_) => {
+            bail!(
+                "Unable to load template {:#?} for {:#?}",
+                metadata.layout, &source
+            );
+        }
+    };
 
-    let content = str::from_utf8(&template)?;
+    let content = str::from_utf8(&template).unwrap_or("");
+ 
+
+    let target = match destination {
+        Some(p) => p.with_extension("html"),
+        None => source.with_extension("html")
+    };
 
     let document = merge_template(content, &metadata, &html);
 
-    let html_file = match File::create(source.with_extension("html")) {
+    let html_file = match File::create(&target) {
         Ok(handle) => handle,
-        Err(error) => bail!(
-            "An error occured while creating the destination HTML file for {:#?} : {}",
-            source,
-            error
-        ),
+        Err(e) => {
+            bail!(
+                "Unable to create the destination file to {:#?} : {:#?}",
+                &target, e
+            );
+        }
     };
 
     let mut writer = BufWriter::new(html_file);
 
-    writer.write(document.as_bytes())?;
+    match writer.write(document.as_bytes()) {
+        Ok(_) => {}
+        Err(e) => {
+            bail!(
+                "Unable to write the rendered file to {:#?} : {:#?}",
+                &target, e
+            );
+        }
+    };
 
     Ok(source.with_extension("html"))
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
     path::{Path, PathBuf, MAIN_SEPARATOR},
 };
 
-use convert::publish;
+use convert::{markdown_to_html};
 use devserver::DevServer;
 use log::info;
 use tokio::{
@@ -19,22 +19,22 @@ use tokio::{
 async fn main() {
     pretty_env_logger::init();
 
+    let root_path = PathBuf::from("G:\\Workspaces\\Rust\\micro\\wwwroot\\");
+    let templates_path = PathBuf::from("G:\\Workspaces\\Rust\\micro\\templates\\");
+
+    start(root_path, templates_path).await;
+}
+
+async fn start(root_path: PathBuf, templates_path: PathBuf) {
     let (sender, mut receiver): (Sender<String>, Receiver<String>) =
         tokio::sync::mpsc::channel(100);
 
-    let root_path = PathBuf::from("G:\\Workspaces\\Rust\\micro\\wwwroot\\");
     let root_watcher = watcher::make_fs_watcher(root_path.clone(), sender.clone(), true, 1000);
-
-    let templates_path = PathBuf::from("G:\\Workspaces\\Rust\\micro\\templates\\");
     let templates_watcher = watcher::make_fs_watcher(templates_path.clone(), sender, true, 1000);
 
     let server = DevServer::new();
     let server_task = server.serve(root_path.clone(), 4200, true, None);
     let clients = server.clients();
-
-    let (pipeline_sender,  pipeline_receiver): (Sender<PathBuf>, Receiver<PathBuf>) =
-        tokio::sync::mpsc::channel(100);
-    let pipeline = convert::create_markdown_pipeline(pipeline_receiver, templates_path.clone());
 
     let consumer = tokio::task::spawn(async move {
         loop {
@@ -43,7 +43,7 @@ async fn main() {
                 info!("File {} changed", &text);
                 let file_path = Path::new(&text);
 
-                if file_path.starts_with(&templates_path) {
+                if file_path.starts_with(templates_path.to_path_buf()) {
                     // Will have to deal with template changes and maybe issue a reload command
                     continue;
                 }
@@ -54,8 +54,11 @@ async fn main() {
                 };
 
                 if extension == "md" {
-                    pipeline_sender.send(file_path.to_path_buf()).await.unwrap();
-                    if let Ok(html) = publish(file_path.to_path_buf()) {
+                    if let Ok(html) = markdown_to_html(
+                        file_path.to_path_buf(),
+                        None,
+                        templates_path.to_path_buf(),
+                    ) {
                         if let Ok(p) = html.strip_prefix(&root_path) {
                             let str = p.to_str().unwrap();
                             devserver::send_message(&clients, str.replace(MAIN_SEPARATOR, "/"))
@@ -67,11 +70,5 @@ async fn main() {
         }
     });
 
-    let (_, _, _, _, _) = join!(
-        templates_watcher,
-        root_watcher,
-        consumer,
-        server_task,
-        pipeline
-    );
+    let (_, _, _, _) = join!(templates_watcher, root_watcher, consumer, server_task);
 }
